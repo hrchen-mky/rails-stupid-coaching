4e4746b9203e477a14619ca2cf4102990d6e52ba
Huiran Chen <huiran.chen@gmail.com>
2019-08-12 16:23:11 -0400

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/ask', to: 'pages#ask', as: :ask
-  post '/ask', to: 'pages#create'
-  get '/answer', to: 'pages#answer', as: :answer
-
-  # Generic syntax:
-  # verb 'path', to: 'controller#action', as: :route_name
 end
